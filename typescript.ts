1a4f1ede9559f63cd97565da34e98160978c6996
IsacHolstad <wisac22@gmail.com>
2022-02-07 12:28:00 +0100

    dc

---
+++ typescript.ts
@@ -0,0 +1,11 @@
+const user = {
+    name: "isac",
+    age: 19
+}
+
+//testing
+class Auther {
+    constructor (public Name: string) {  }
+}
+let Auther = new Auther("kim danial");
+console.log("/hello readers")
