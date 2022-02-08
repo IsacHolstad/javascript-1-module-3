2ed198cf6981b621dd6d84bab206ca16b424af74
IsacHolstad <wisac22@gmail.com>
2022-02-08 12:10:39 +0100

    fg

--- training-afterclass/javascript.js
+++ training-afterclass/javascript.js
@@ -1,5 +1,40 @@
 //examples from presentasion 7.feb 13:50pm
+function someFunction() {
+    console.log("hello")
+}
+someFunction()
 
+
+
+
+const favoritPlace = "Fauske";
+    function showFavortPlace () {
+        console.log("favorit place is : " + favoritPlace)
+    }
+function isFavorit() {
+    if(favoritPlace === "Fauske") {
+        console.log(favoritPlace +" is my favorit place on earth")
+    }
+    else {
+        console.log("is not my favort place")
+    }
+}
+isFavorit()
+
+const favoritCar = "TESLA";
+function ShowFavoritCar () {
+    if(favoritCar === "TESLA") {
+        console.log(favoritCar +" is my favorit car")
+    }else{
+        console.log("is not my favorit car")
+    }
+}
+ShowFavoritCar()
+
+//logMessage()
+//function logMessage() {
+   // console.log("bruh automativ")
+//}
 
 
  
