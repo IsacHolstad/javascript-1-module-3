ecd0981b921e3a89a0f43702e4723c29679c4438
IsacHolstad <wisac22@gmail.com>
2022-02-08 11:54:50 +0100

    11:54

--- training-afterclass/javascript.js
+++ training-afterclass/javascript.js
@@ -1,11 +1,8 @@
 //examples from presentasion 7.feb 13:50pm
 
-const header = document.querySelector(".header");
-header.style.color = "white";
-header.style.fontSize = "20px";
 
-{
-    const 
-}
+
+ 
+
 
 
