0d748ca2cdb45c9afa49f487e3ed1c4c9e1ef6e2
IsacHolstad <wisac22@gmail.com>
2022-02-10 13:13:33 +0100

    13:13

---
+++ lesson-4/get-query-parameters/script.js
@@ -0,0 +1,31 @@
+//example 1
+console.log(document.location);
+
+//how to get the query param
+
+console.log(document.location.search)
+
+const queryString = document.location.search;
+//in order t oget the value
+const params = new URLSearchParams(queryString);
+console.log(params)
+console.log(params.get("school"));//this whil show in consol "NOROFF"
+const mySchoolParams = params.get("school");
+
+//i want to chack that i hava param called school
+//console.log i hava the param
+
+//i want to chack this param is equal to noroff
+//okay thee param is noroff
+
+//else redircet to another page
+
+if(params.has("school")) {
+    console.log("i have the param")
+}
+else if(param.get("school")=== "noroff") {
+    console.log("okey the param is noroff")
+}
+else{
+    document.location.href = "second.html";
+}
